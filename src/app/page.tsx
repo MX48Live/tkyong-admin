247c0242834b048e68fe79e1f00418fcf4174731
Watee K <Watee.K@link-th.com>
2023-07-03 17:10:55 +0700

    Add Initital

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import "@/styles/Main.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
